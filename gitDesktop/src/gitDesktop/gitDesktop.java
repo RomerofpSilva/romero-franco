568d96a6a0aa96f0576a8ab0e02291f1e9e6a15e
Romero_Franco <romerofpsilva@gmail.com>
2026-02-19 21:11:50 -0300

    Adiciona ex1

--- gitDesktop/src/gitDesktop/gitDesktop.java
+++ gitDesktop/src/gitDesktop/gitDesktop.java
@@ -1,10 +1,31 @@
 package gitDesktop;
 
+import java.util.Scanner;
+
 public class gitDesktop {
 
 	public static void main(String[] args) {
 		
-
+		Scanner romero = new Scanner (System.in);
+		
+		System.out.println("Informe o número da matricula: ");
+		String mat = romero.next();
+		System.out.println("Informe seu nome: ");
+		String nome = romero.next();
+		System.out.println("Descreva a quantidade de horas trabalhadas: ");
+		int hora = romero.nextInt();
+		System.out.println("Digite o valor da sua hora de trabalho: ");
+		double valorHora = romero.nextDouble();
+		
+		double preco = hora * valorHora;
+		double salfinal = preco * 28;
+		
+		System.out.println("O salaria mensal esta no valor de: "+ salfinal);
+		System.out.println("Número da matricula: " + mat);
+		System.out.println("Nome: " + nome);
+				
+				
+		romero.close();
 	}
 
 }
